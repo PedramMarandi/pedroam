b6e5be97ee795cddc31c19e67b30adc88d440fa9
Pedram marandi <pedram.marandi@gmail.com>
2019-09-01 22:33:09 +1000

    Fix in blog config

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
+  title: 'Pedroam | Javascript Developer', // Required
+  author: 'Pedram Marandi', // Required
   description: 'Full-stack Web Developer',
   primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+    website: 'https://pedroam.dev',
+    github: 'https://github.com/PedramMarandi',
+    twitter: 'https://twitter.com/pedram_marandi',
+    linkedin: 'https://ca.linkedin.com/in/pedram_marandi',
   },
-  pathPrefix: '/devblog',
-  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+  pathPrefix: '/',
+  siteUrl: 'https://pedroam.dev',
 };
